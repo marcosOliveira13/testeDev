7851f86a99a568ec0bcdda4cbad4d4d71babb90a
Marcos <marukosu05@gmail.com>
2021-05-18 15:54:56 -0300

    painel de vizualizacao

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{route('mostra_aluno')}}">Alunos</a>
+    <a href="{{route('mostra_cursos')}}">Cursos</a>
+    <a href="{{route('mostra_matricula')}}">Matricula</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 route::get('/aluno/novo','AlunosController@create')->name('novo_aluno');
 route::post('/aluno/novo','AlunosController@store')->name('salvar_aluno');
 //mostrar alunos cadastrados 
-route::get('/aluno/ver','AlunosController@show');
+route::get('/aluno/ver','AlunosController@show')->name('mostra_aluno');
 //excluir alunos cadastrados
 route::get('/aluno/del/{id}','AlunosController@destroy')->name('excluir_aluno');
 //editar alunos cadastrados 
@@ -33,7 +33,7 @@ route::post('/aluno/edit/{id}','AlunosController@update')->name('atualizar_aluno
 route::get('/curso/novo','CursosController@create')->name ('novo_curso');
 route::post('/curso/novo','CursosController@store')->name('salvar_curso');
 //mostrar cursos cadastrados 
-route::get('/curso/ver','CursosController@show');
+route::get('/curso/ver','CursosController@show')->name('mostra_cursos');
 //excluir cursos cadastrados
 route::get('/curso/del/{id}','CursosController@destroy')->name('excluir_curso');
 //editar curso
@@ -42,9 +42,7 @@ route::post('/curso/edit{id}','CursosController@update')->name('atualizar_curso'
 //reliza matricula
 route::get('/matricula/nova','MatriculaController@create')->name ('nova_matricula');
 route::post('/matricula/nova','MatriculaController@store')->name('salvar_matricula');
-route::get('/matricula/ver','MatriculaController@show');
+//mostra matricula
+route::get('/matricula/ver','MatriculaController@show')->name('mostra_matricula');
 //eclui matricula
 route::get('/matricula/del/{id}','MatriculaController@destroy')->name('excluir_matricula');
-//editar curso
-route::get('/matricula/edit/{id}','MatriculaController@edit')->name('editar_matricula');
-route::post('/matricula/edit/{id}','MatriculaController@edit')->name('atualizar_matricula');
